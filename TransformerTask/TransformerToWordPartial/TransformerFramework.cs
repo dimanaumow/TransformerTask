481285ada86fa61c966faa649c2910798f391de6
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2020-08-15 18:26:33 +0300

    fix a few name bags

--- TransformerTask/TransformerToWordPartial/TransformerFramework.cs
+++ TransformerTask/TransformerToWordPartial/TransformerFramework.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TransformerToWordPartial
 {
-    public class Class1
+    public partial class Transformer
     {
+        public string[] Transform(double[] source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} cannot be null.");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(source)} cannot be empty.");
+            }
+
+            var result = new string[source.Length];
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                TransformByPredicate(result, source[i]);
+            }
+
+            return result;
+        }
+
+        static partial void TransformByPredicate(ICollection<string> collection, double item);
     }
 }
