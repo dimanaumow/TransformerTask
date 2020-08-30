9d7e8ef4690ffc017820c1811414de78d925b601
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2020-08-30 19:15:08 +0300

    feat: add Transfomer inheritance

---
+++ TransformerTask/TransformerInheritance/Transformer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TransformerInheritance
+{
+    public abstract class Transformer
+    {
+        public string[] Transform(double[] source)
+        {
+
+            if (source is null)
+            {
+                throw new ArgumentNullException($"{nameof(source)} cannot be null.");
+            }
+
+            if (source.Length == 0)
+            {
+                throw new ArgumentException($"{nameof(source)} cannot be empty.");
+            }
+
+            var result = new string[source.Length]; 
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                result[i] = TransformAccordingToRule(source[i]);
+            }
+
+            return result;
+        }
+
+        protected abstract string TransformAccordingToRule(double item);
+    }
+}
