e8915d160f8906ee1bc42755322d23dea0cd423f
Dzmitry Naumov <dzmitriy.naumov@gmail.com>
2020-08-30 15:49:13 +0300

    fix style cop warning

--- TransformerTask/TransformerToWordPartial/TransformerFramework.cs
+++ TransformerTask/TransformerToWordPartial/TransformerFramework.cs
@@ -3,8 +3,18 @@ using System.Collections.Generic;
 
 namespace TransformerToWordPartial
 {
+    /// <summary>
+    /// Implement transformer class.
+    /// </summary>
     public partial class Transformer
     {
+        /// <summary>
+        /// Transform each element of source array into its "word format".
+        /// </summary>
+        /// <param name="source">Source array.</param>
+        /// <returns>Array of "word format" of elements of source array.</returns>
+        /// <exception cref="ArgumentNullException">Throw if array is null.</exception>
+        /// <exception cref="ArgumentException">Throw if array is empty.</exception>
         public string[] Transform(double[] source)
         {
             if (source is null)
